f6bc1fa9b7515a5139a07ef64c30d5cade99ff0f
Felipe Galera Martinez <fgm.si@cbs.dk>
2020-02-18 14:24:17 +0100

    Updated os

--- 1. Linear regression/py_files/regression.py
+++ 1. Linear regression/py_files/regression.py
@@ -7,10 +7,13 @@ from sklearn import linear_model
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 import pickle
+import os
+
 
 # Read data
 
-data = pd.read_csv("C:/Users/fgm.si/Documents/GitHub/ML & AI Megacourse/1. Linear regression/raw_data/student-mat.csv", sep = ";")
+os.chdir("C:/Users/fgm.si/github/ML-AI-Megacourse/1. Linear regression")
+data = pd.read_csv("raw_data/student-mat.csv", sep = ";")
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
 # Separating Our Data
@@ -26,13 +29,13 @@ X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X,y,
 best = 0
 for _ in range(100):
     #Running a 100 regressions
-    
+
     X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X,y, test_size = 0.1)
     linear_regression = linear_model.LinearRegression()
     linear_regression.fit(X_train, y_train)
     acc = linear_regression.score(X_test, y_test)
     print("Accuracy:" + str(acc))
-    
+
     #Keeping the best score
     if acc > best:
         best = acc
@@ -41,7 +44,7 @@ for _ in range(100):
             pickle.dump(linear_regression, f)
 
 # Viewing The Constants
-            
+
 print("-------------------------")
 print( "Coefficients: \n", linear_regression.coef_)
 print("-------------------------")
@@ -54,9 +57,9 @@ predictions = linear_regression.predict(X_test)
 
 for i in range(len(predictions)):
     print(predictions[i], y_test[i], X_test[i])
-    
+
 # Drawing and plotting model
-    
+
 plt.style.use("ggplot")
 plot = "G2"
 plt.scatter(data[plot], data["G3"])
@@ -64,7 +67,7 @@ plt.xlabel(plot)
 plt.ylabel("Final Grade")
 plt.show()
 
-"""  
+"""
 # Instead of linear_regression you can just load the model from pickle
 
 pickle_in = open("studentgrades.pickle", "rb")
