1c5c4527f70fe743c11a5b1867df9221feaf33e6
Pipe Galera <pipegalera@gmail.com>
2020-02-28 17:37:50 +0100

    Update svm.py

--- 3. Support Vector Machines (SVM)/py_files/svm.py
+++ 3. Support Vector Machines (SVM)/py_files/svm.py
@@ -3,11 +3,61 @@
 Created on Mon Feb  28 2020
 Author: Pipe Galera
 Project: Introduction to SVM
-Goal: classify cars in 4 categories based upon certain features.
+Goal: classify breast cancer tumors into malignant or benign.
 Data: sklearn
 
 """
 
 # Import modules
-import sklearn
+import numpy as np
 import pandas as pd
+import sklearn
+from sklearn import datasets, svm, metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+# Load datasets
+cancer_data = datasets.load_breast_cancer()
+
+# Implementation
+X = cancer_data.data
+y = cancer_data.target
+
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X,y, test_size = 0.2)
+
+classes = ["malignant", "benign"]
+
+# Suport vector Machine
+""" Takes forever to compute if you try different kernels
+best_score = 0
+for i in ["poly", "rbf", "sigmoid"]:
+    clf = svm.SVC(kernel = "poly", gamma = "auto")
+    clf.fit(X_train, y_train)
+    y_predict = clf.predict(X_test)
+    acc = metrics.accuracy_score(y_test, y_predict)
+    print("Accuracy " + "with kernel " + str(i) + " is " + str(acc))
+    # Keep higher accuracy
+    if acc > best_score:
+        best_score = acc
+        best_kernel = i
+print("Best cccuracy: " + str(best_score) + " reached with " + str(best_kernel) + " Kernel")
+"""
+
+clf_svm = svm.SVC(kernel = "linear", C=2, gamma = "scale")
+clf_svm.fit(X_train, y_train)
+y_predict = clf_svm.predict(X_test)
+acc = metrics.accuracy_score(y_test, y_predict)
+print(acc)
+
+# K- Nearest Neighbors
+best_score = 0
+for i in range(1,100):
+    knn = KNeighborsClassifier(n_neighbors = i)
+    knn.fit(X_train, y_train)
+    y_predict = knn.predict(X_test)
+    acc = metrics.accuracy_score(y_test, y_predict)
+    # Highest Accuracy
+    if acc > best_score:
+        best_score = acc
+        best_neighbors = i
+
+print("Best acccuracy " + str(best_score) + " reached with " + str(best_neighbors) + " Neighbors")
