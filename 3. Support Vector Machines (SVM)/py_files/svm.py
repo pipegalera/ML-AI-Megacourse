03de22795adc087fffd46f52a1b9c5108fa5cf89
pipegalera <pipegalera@gmail.com>
2020-02-28 14:19:30 +0100

    updated new course

---
+++ 3. Support Vector Machines (SVM)/py_files/svm.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  28 2020
+Author: Pipe Galera
+Project: Introduction to SVM
+Goal: classify cars in 4 categories based upon certain features.
+Data: sklearn
+
+"""
+
+# Import modules
+import sklearn
+import pandas as pd
